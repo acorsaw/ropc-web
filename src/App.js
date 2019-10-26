db84a8c4c2de87a797897b7dd3d3321953edf78a
Adam Corsaw <adam@corsaw.com>
2019-10-26 17:52:01 -0500

    Working on secured components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { withAuthenticator } from 'aws-amplify-react'
+import Amplify from 'aws-amplify'
+import { Auth } from 'aws-amplify'
+import awsconfig from './aws-exports'
+
+Amplify.configure(awsconfig)
+
+
+let email = null
+
+function signIn() {
+  Auth.federatedSignIn()
+}
+
+function checkUser() {
+  Auth.currentAuthenticatedUser()
+  .then(user => console.log({user}))
+  .catch(err => console.log(err))
+}
+
+function signOut() {
+  Auth.signOut()
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+}
+
+function getUserEmail() {
+  Auth.currentUserInfo()
+    .then(user => console.log({user}))
+    .catch(err => console.log({err}))
+}
+
 function App() {
   return (
     <div className="App">
@@ -10,17 +42,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={signOut}>Sign Out</button>
       </header>
     </div>
   );
 }
 
-export default App;
+const federatedConfig = {
+  google_client_id: '1032227651519-e2c4uantc6vk7u9i7inqkb13t5fm0va1.apps.googleusercontent.com'
+}
+
+export default withAuthenticator(App, {federated: federatedConfig});
